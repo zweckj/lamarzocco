6db12ed45d077ddabcf3143511ab6c4e4f416bfd
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2023-10-02 12:15:50 +0200

    remove old tests

--- tests/test_init.py
+++
@@ -1,9 +0,0 @@
-"""Test La Marzocco component setup."""
-from homeassistant.setup import async_setup_component
-
-from custom_components.lamarzocco.const import DOMAIN
-
-
-async def test_async_setup(hass, enable_custom_integrations):
-    """Test the component gets setup."""
-    assert await async_setup_component(hass, DOMAIN, {}) is True
